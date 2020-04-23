2c8328b548ad8a9348a98b1f9afaf4da9bb06425
unknown <kim.taehoon@nifty.co.jp>
2020-04-23 17:41:39 +0900

    create test code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
+function Potato(){
+  return <h1>I hate potato</h1>
+}
 function App() {
-  return <div>Hello!</div>;
+  return (
+  <div>
+    <h1>Hello!</h1>
+    <Potato />
+  </div>
+  );
 }
 
 export default App;

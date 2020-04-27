a71b2687a717165b540e455db51700cfe59532cf
unknown <kim.taehoon@nifty.co.jp>
2020-04-27 17:44:47 +0900

    create componentDidMount

--- src/App.js
+++ src/App.js
@@ -3,26 +3,20 @@ import React from 'react';
 // React call render() component from class
 class App extends React.Component{
   state = {
-    count:0
+    isLoading: true
   };
-  add =() =>{
-    this.setState(current => ({count: current.count + 1}));
-  };
-  minus =() =>{
-    this.setState(current => ({count: current.count - 1}));
+  componentDidMount(){
+    // delay function
+    setTimeout(() => {
+      this.setState({isLoading: false});
+    })
 
-  };
-  render(){
-    return (
-      <div>
-        <h1> The number is:{this.state.count} </h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
-      </div>
-    )
-      
   }
-
+  render(){
+    console.log("I'm rendering");
+    // ES6
+    const { isLoading } = this.state;
+    return <div>{isLoading ? "Loading" : "We are ready"}</div>;
+  }
 }
-
 export default App;
